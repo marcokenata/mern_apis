e22772eed4751caa055b04ac037d09612ca86a0b
marcokenata <zetplane162@gmail.com>
2020-10-29 21:32:52 +0700

    modify index and changing commands

--- express/index.js
+++ express/index.js
@@ -27,7 +27,7 @@ app.use((req, res, next) => {
 
 app.use(body_parser.json());
 
-app.use('/.netlify/functions/server', routes);
+app.use('/.netlify/functions/index', routes);
 
 app.use((err, req, res, next) => {
     console.log(err);
