be0d0486dd479dbfb2b46387002ed29de80a8727
marcokenata <zetplane162@gmail.com>
2020-11-01 10:50:02 +0700

    add control allow method cors, change from stars into methods

--- express/index.js
+++ express/index.js
@@ -22,7 +22,7 @@ mongoose.Promise = global.Promise;
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,POST,DELETE");
     next();
 });
 
